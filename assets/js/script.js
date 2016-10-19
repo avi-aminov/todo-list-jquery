c4610c5bad8fd67579def7611c6fa5b6404ad708
avi-aminov <aviaminov.co.il@gmail.com>
2016-10-19 07:43:02 +0200

    basic closures

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-ÿþ
+var App = (function () {
+
+    var privateMethod = function () {};
+    
+    var htmlTask = function (taskValue) {
+        return '<li class="task-wrap"><div class="checkbox"> <label> <input class="checkbox-task" type="checkbox" value="">  '+ taskValue +'  </label>  <button type="button" class="btn btn-default delete-task"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></div></li>';
+    };
+    
+    return {
+        
+        init: function(){
+            
+        },
+
+        addNewTask: function(val){
+            jQuery('#task-list').append(htmlTask(val));
+        },
+    };
+    
+})();
+
+
+
+
+jQuery('#add-task-btn').click(function(){
+    var taskValue = jQuery('#add-task-input').val();
+    if(taskValue != ''){
+        App.addNewTask(taskValue);
+    }
+});
