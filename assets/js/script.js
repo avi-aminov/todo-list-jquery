1e3a8f656eb5457ccb83b453a07f32423798ec4b
avi-aminov <aviaminov.co.il@gmail.com>
2016-10-19 16:48:17 +0200

    extra function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 var App = (function () {
-
+    'use strict';
     var privateMethod = function () {};
     
-    var htmlTask = function (taskValue) {
-        return '<li class="task-wrap"><div class="checkbox"> <label> <input class="checkbox-task" type="checkbox" value="">  '+ taskValue +'  </label>  <button type="button" class="btn btn-default delete-task"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></div></li>';
+    var makeid = function () {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 5; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+    }
+    
+    var htmlTask = function (id, text, checked) {
+        return '<li id="id-'+id+'" class="task-wrap"><div class="checkbox"> <label> <input class="checkbox-task" type="checkbox" value="" '+ (checked ? "checked" : "") +'><apan class="task-text '+ (checked ? "is_checked" : "") +'">'+ text +' </span> </label>  <button type="button" class="btn btn-default delete-task"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></div></li>';
     };
+    
+    var data = [];
     
     return {
         
@@ -13,13 +24,22 @@ var App = (function () {
         },
 
         addNewTask: function(val){
-            jQuery('#task-list').append(htmlTask(val));
+            
+            data.push({
+                    id:makeid(),
+                    text:val,
+                    is_checked: true });
+            
+            jQuery('#task-list').html("");
+            data.forEach(function (value, key) {
+                console.log(value);
+                jQuery('#task-list').append(htmlTask(value.id, value.text, value.is_checked));
+            });
+            jQuery('#add-task-input').val("");
         },
     };
     
 })();
-
-
 
 
 jQuery('#add-task-btn').click(function(){
@@ -28,3 +48,14 @@ jQuery('#add-task-btn').click(function(){
         App.addNewTask(taskValue);
     }
 });
+
+
+jQuery( "#task-list" ).on( "click", ".checkbox-task", function() {
+    var _this = jQuery(this);
+    var is_checked = _this.is(':checked');   
+    var _wrap = _this.closest(".task-wrap");
+    var _label = _wrap.find('.task-text');
+
+    _label.toggleClass("is_checked", is_checked);
+});
+
